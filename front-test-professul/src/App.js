4d82dea9ee85a455604e6ef2fdfa260e762f0074
Jisu <139774652+serendipity63@users.noreply.github.com>
2024-03-01 15:52:20 +0900

    proflist

--- front-test-professul/src/App.js
+++ front-test-professul/src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RootLayout from "./Root";
 import Home from "./pages/Home/Home";
+import ProfList from "./pages/List/ProfList";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <ProfList />
     </div>
   );
 }
---
+++ front-test-professul/src/pages/List/List.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
+const List = () => {
+  return (
+    <div>
+      <h1>My Data Table</h1>
+      <DataTable>
+        <Column field="name" header="Name"></Column>
+        <Column field="age" header="Age"></Column>
+      </DataTable>
+    </div>
+  );
+};
+
+export default List;
