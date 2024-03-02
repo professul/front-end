783b687d620e86e4b74e220db65087f348ccc9ca
Jisu <139774652+serendipity63@users.noreply.github.com>
2024-03-02 15:42:32 +0900

    signup&login form design completed

--- front-test-professul/src/App.js
+++ front-test-professul/src/App.js
@@ -5,12 +5,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import ProfList from "./pages/List/ProfList";
 import ReviewContent from "./pages/Review/ReviewContent";
+import SignUp from "./pages/SignUp/Signup";
+import LoginForm from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/login" element={<LoginForm />} />
         <Route exact path="/professor" element={<ProfList />} />
         <Route exact path="/professor/review" element={<ReviewContent />} />
       </Routes>
