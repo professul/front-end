f80f5b6e3f4b48be94447c1bd6cfea4f81d07c24
JuhyeonLee1 <zugusdl@gmail.com>
2024-03-03 21:02:24 +0900

    비교 페이지

--- front-test-professul/src/App.js
+++ front-test-professul/src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home/Home";
 import ProfList from "./pages/List/ProfList";
 import ReviewContent from "./pages/Review/ReviewContent";
 import Rate from "./pages/Review/Rate";
+import Compare from "./pages/Compare/Compare";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/professor" element={<ProfList />} />
         <Route exact path="/professor/review" element={<ReviewContent />} />
         <Route exact path="/rating" element={<Rate />} />
+        <Route exact path="/professor/compare" element={<Compare />} />
       </Routes>
     </div>
   );
